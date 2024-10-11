8e60ff3c58167e10eed6b79d5952a7ac583ba8ea
БАРУЛИН МАКСИМ АЛЕКСЕЕВИЧ <Barulin.MA@ashipyards.com>
2024-10-11 14:32:39 +0300

    по новой (отрезка+(легкая правка(хоть живет)))

--- example_calculator/calc_drag_listbox.py
+++ example_calculator/calc_drag_listbox.py
@@ -24,7 +24,7 @@ class QDMDragListbox(QListWidget):
         #print(CALC_NODES)
         keys = list(CALC_NODES.keys())
         keys.sort()
-        ex_list = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 26, 27, 28]
+        ex_list = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 26, 27, 28, 29]
         for key in keys:
             if key in ex_list:
                 continue
@@ -91,7 +91,7 @@ class QDMDragListbox1(QListWidget):
         print(CALC_NODES)
         keys = list(CALC_NODES.keys())
         keys.sort()
-        ex_list = [1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+        ex_list = [1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
         for key in keys:
             if key in ex_list:
                 continue
@@ -225,7 +225,7 @@ class QDMDragListbox3(QListWidget):
         print(CALC_NODES)
         keys = list(CALC_NODES.keys())
         keys.sort()
-        ex_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+        ex_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
         for key in keys:
             if key in ex_list:
                 continue
